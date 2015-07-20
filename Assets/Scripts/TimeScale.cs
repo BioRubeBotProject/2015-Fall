df10346ed1d8c0378ba1801b88971a82e23f034a
J21TheSender <jrogers4@my.athens.edu>
2015-07-20 00:39:43 -0500

    First Commit
    -This is the final Commit from Spring-2015

---
+++ Assets/Scripts/TimeScale.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScale : MonoBehaviour 
+{
+	public void Start()
+	{
+		Time.timeScale = 0;
+	}
+	
+	public void PauseButton()
+	{
+		doPauseToggle ();
+	}
+	public void FastForwardButton()
+	{
+		doFastForward ();
+	}
+
+	public void PlayButton()
+	{
+		doPlay ();
+	}
+	void doPlay()
+	{
+		Time.timeScale = 1;
+	}
+	void doPauseToggle()
+	{
+		if (Time.timeScale > 0) 
+		{
+			Time.timeScale = 0;
+		} 
+		else 
+		{
+			Time.timeScale = 1;
+		}
+	}
+	void doFastForward()
+	{
+		if (Time.timeScale >= 1 && Time.timeScale < 2) 
+		{
+			Time.timeScale = Time.timeScale + 1;
+		}
+		else 
+		{
+			Time.timeScale = 1;		
+		}
+
+	}
+	
+}
